c037f12a673e014e4c3ba45dd144ce6513e1a7f0
Ripto <109461996+NathanNeurotic@users.noreply.github.com>
2024-12-13 07:45:23 -0800

    Reverted Tags - Fixed Search - Added Links
    Still needs re-structuring

--- script.js
+++ script.js
@@ -11,15 +11,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const searchBar = document.getElementById("search-bar");
     searchBar.addEventListener("input", () => {
         const searchQuery = searchBar.value.toLowerCase();
-        const tagsQuery = searchBar.value.toLowerCase();
         const sections = document.querySelectorAll("section");
         sections.forEach((section) => {
             const links = section.querySelectorAll("a");
             let sectionVisible = false;
             links.forEach((link) => {
                 const linkText = link.textContent.toLowerCase();
-                const tags = link.getAttribute("data-tags").toLowerCase();
-         if (linkText.includes(searchQuery) || tags.includes(tagsQuery)) {
+                if (linkText.includes(searchQuery)) {
                     link.style.display = "block";
                     sectionVisible = true;
                 } else {
